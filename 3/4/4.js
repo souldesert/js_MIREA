835ebe4de9be2f3e350b6320c61fb317e9c782df
a.vasilkov <a.vasilkov@voskhod.ru>
2018-06-08 11:23:52 +0300

    Сделано.

---
+++ 3/4/4.js
@@ -0,0 +1,21 @@
+function Calculator() {
+
+    this.read = function() {
+        this.a = +prompt('a?', 0);
+        this.b = +prompt('b?', 0);
+    };
+
+    this.sum = function() {
+        return this.a + this.b;
+    };
+
+    this.mul = function() {
+        return this.a * this.b;
+    };
+}
+
+var calculator = new Calculator();
+calculator.read();
+
+alert("Сумма = " + calculator.sum());
+alert("Произведение = " + calculator.mul());
